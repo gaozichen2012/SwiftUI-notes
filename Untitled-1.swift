1fe0c3018ec58cc514632c321e22805d7ed82769
Tom Gao <gaozichen2012@outlook.com>
2019-11-26 00:26:11 +0800

    Update Untitled-1.swift

--- Untitled-1.swift
+++ Untitled-1.swift
@@ -1,40 +1,54 @@
+//
+//  ContentView.swift
+//  TomSwiftUITest
+//
+//  Created by Tom on 24/11/2019.
+//  Copyright © 2019 Tom. All rights reserved.
+//
+
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false
     var body: some View {
         ZStack {
-            VStack {
-                Text("card back")
+            TitleView()
+                .blur(radius: show ? 20 : 0)
+             .animation(.default)
+            CardBottonView()
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
+            
+            CardView()
+                .background(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/)
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -400 : -40)
+                .scaleEffect(0.85)
+                .rotationEffect(Angle(degrees: show ? 15 : 0))
+//                .rotation3DEffect(Angle(degrees: show ? 50 : 0), axis: /*@START_MENU_TOKEN@*/(x: 10.0, y: 10.0, z: 10.0)/*@END_MENU_TOKEN@*/)
+                .blendMode(.hardLight)
+            .animation(.easeInOut(duration: 0.5))
+            CardView()
+                .background(show ? Color.red : Color("backgrand8"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -200 : -20)
+                .scaleEffect(0.9)
+                .rotationEffect(Angle(degrees: show ? 10 : 0))
+//                .rotation3DEffect(Angle(degrees: show ? 40 : 0), axis: /*@START_MENU_TOKEN@*/(x: 10.0, y: 10.0, z: 10.0)/*@END_MENU_TOKEN@*/)
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.3))
+                //.animation(.easeIn(duration: 2,curve:.easeIn))
+            CertificateView()
+                .scaleEffect(0.95)
+                .rotationEffect(Angle(degrees: show ? 5 : 0))
+//                .rotation3DEffect(Angle(degrees: show ? 30 : 0), axis: /*@START_MENU_TOKEN@*/(x: 10.0, y: 10.0, z: 10.0)/*@END_MENU_TOKEN@*/)
+                .animation(.spring())
+                .onTapGesture {
+                    self.show.toggle()
             }
-            .frame(width: 300, height: 220.0)
-            .background(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/)
-                
-            .cornerRadius(20)
-            .shadow(radius: 20)
-            .offset(x: 0, y: -20)
-            VStack {
-                HStack {
-                    VStack {
-                        VStack(alignment: .leading) {
-                            Text("UI Design")
-                                .font(.headline).fontWeight(.medium).foregroundColor(Color.green).padding(.top)
-                            Text("Tom")
-                                .foregroundColor(Color.white)
-                        }
-                    }
-                    Spacer()
-                    Image("Logo")
-                        .resizable()
-                        .frame(width: 30.0, height: 30.0)
-                }
-                .padding(.horizontal)
-                Spacer()
-                Image("Background")
-            }
-            .frame(width: 340.0, height: 220.0)
-            .background(Color.black)
-            .cornerRadius(20)
-            .shadow(radius: 20)
+            
         }
     }
 }
@@ -42,5 +56,79 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            Text("card back")
+        }
+        .frame(width: 340, height: 220.0)
+
+    }
+}
+
+struct CertificateView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack {
+                    VStack(alignment: .leading) {
+                        Text("UI Design")
+                            .font(.headline).fontWeight(.medium).foregroundColor(Color.green).padding(.top)
+                        Text("Tom")
+                            .foregroundColor(Color.white)
+                    }
+                }
+                Spacer()
+                Image("Logo")
+                    .resizable()
+                    .frame(width: 30.0, height: 30.0)
+            }
+            .padding(.horizontal)
+            Spacer()
+            Image("Background")
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Tom")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                Spacer()
+            }
+            Image("Illustration5")
+            Spacer()
+        }.padding()
+    }
+}
+
+struct CardBottonView: View {
+    var body: some View {
+        VStack(spacing: 20.0) {
+            Rectangle()
+                .frame(width: 60, height: 6)
+                .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                .opacity(0.1)
+            Text("This is tom test ui design,and this is a long long long long long text")
+                .lineLimit(5)
+            Spacer()
+        }
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .padding()
+        .padding(.horizontal)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .offset(y: 600)
     }
 }
