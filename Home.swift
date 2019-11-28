9d3297607db19de32ce30822c63fcd9af3f6e207
Tom Gao <gaozichen2012@outlook.com>
2019-11-28 23:44:36 +0800

    none

--- Home.swift
+++ Home.swift
@@ -9,22 +9,26 @@
 import SwiftUI
 
 struct Home: View {
+    
+    @State var show = false
+    @State var showProfile = false
     var body: some View {
-        VStack(alignment: .leading, spacing: 20.0) {
-            MenuRow(image: "person.crop.circle", text: "My Account")
-            MenuRow(image: "creditcard", text: "Billing")
-            MenuRow(image: "person.and.person", text: "Team")
-            MenuRow(image: "arrow.uturn.down", text: "Sign out")
-            Spacer()
+        ZStack {
+
+            ContentView()
+                .background(Color.white)
+                .cornerRadius(30)
+                .shadow(radius: 20)
+                .animation(.spring())
+                .offset(y: showProfile ? 0 : UIScreen.main.bounds.height)
+            MenuButton(show: $show)
+                .offset(x: -30, y: showProfile ? 0 : 80)
+            .animation(.spring())
+            MenuRight(show: $showProfile)
+                .offset(x: -16, y: showProfile ? 0 : 88)
+            .animation(.spring())
+            MenuView(show: $show)
         }
-        .padding(.top,20)
-        .padding(30)
-        .frame(minWidth: 0, maxWidth: .infinity)
-        .background(Color.white)
-        .cornerRadius(30)
-        .padding(.trailing, 60)
-        .shadow(radius: 20)
-        
     }
 }
 
@@ -45,6 +49,92 @@ struct MenuRow: View {
                 .frame(width: 32, height: 32) //目的让图标大小一致
             Text(text)
                 .font(.headline)
+            Spacer()
+        }
+    }
+}
+
+struct Menu : Identifiable {
+    var id = UUID() //每次使用数据模型时都需要此句UUID作为默认值
+    var title : String
+    var icon : String
+}
+
+let menuData = [
+    Menu(title: "My Account", icon: "person.crop.circle"),
+    Menu(title: "Billing", icon: "creditcard"),
+    Menu(title: "Team", icon: "person.and.person"),
+    Menu(title: "Sign out", icon: "arrow.uturn.down"),
+]
+
+struct MenuView: View {
+    var menu = menuData
+    @Binding var show : Bool
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20.0) {
+            ForEach(menu){ item in
+                MenuRow(image: item.icon, text: item.title)
+            }
+            Spacer()
+        }
+        .padding(.top,20)
+        .padding(30)
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+        .padding(.trailing, 60)
+        .shadow(radius: 20)
+        .rotation3DEffect(Angle(degrees: show ? 0 : 60), axis: (x: 0, y: 10.0, z: 0))
+        .animation(.default)
+        .offset(x: show ? 0 : -UIScreen.main.bounds.width)
+    }
+}
+
+struct CircleButton: View {
+    var icon = "person.crop.circle"
+    var body: some View {
+        HStack {
+            Image(systemName: icon)
+                .foregroundColor(.black)
+        }
+        .frame(width: 44, height: 44)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(color: Color("buttonShadow"), radius: 20, x: 0, y: 20)
+    }
+}
+
+struct MenuButton: View {
+    @Binding var show : Bool
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            Button(action: { self.show.toggle() }) {
+                Spacer()
+                Image(systemName: "list.dash")
+                    .foregroundColor(.black)
+            }
+            .padding(.trailing, 20)
+            .frame(width: 90, height: 60)
+            .background(Color.white)
+            .cornerRadius(30)
+            .shadow(color: Color("buttonShadow"), radius: 20, x: 0, y: 20)
+            Spacer()
+        }
+    }
+}
+
+struct MenuRight: View {
+    @Binding var show : Bool
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            HStack {
+                Button(action: { self.show.toggle() }) {
+                    CircleButton(icon: "person.crop.circle")
+                }
+                Button(action: { self.show.toggle() }) {
+                    CircleButton(icon: "bell")
+                }
+            }
             Spacer()
         }
     }
